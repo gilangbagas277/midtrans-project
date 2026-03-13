15ad4336d30d97c77a43736f7ac7cdd749d465c9
gilangbagas277 <gilangbagas140303@gmail.com>
2026-03-13 20:46:40 +0700

    Update payment.js

--- backend/src/routes/payment.js
+++ backend/src/routes/payment.js
@@ -1,1 +1,25 @@
+const router = require("express").Router()
+const snap = require("../midtrans")
 
+router.post("/topup", async (req, res) => {
+
+const orderId = "ORDER-" + Date.now()
+const amount = req.body.amount
+
+const parameter = {
+transaction_details: {
+order_id: orderId,
+gross_amount: amount
+}
+}
+
+const transaction = await snap.createTransaction(parameter)
+
+res.json({
+orderId: orderId,
+snapToken: transaction.token
+})
+
+})
+
+module.exports = router
