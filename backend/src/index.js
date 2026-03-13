3870fa56822cd5f68092b83abe416c9f6bcb2859
gilangbagas277 <gilangbagas140303@gmail.com>
2026-03-13 20:45:12 +0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,17 @@
+const express = require("express")
+const cors = require("cors")
 
+const paymentRoutes = require("./routes/payment")
+const webhook = require("./webhook")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/payments", paymentRoutes)
+app.post("/midtrans-webhook", webhook)
+
+app.listen(3000, () => {
+console.log("Server running")
+})
